6a72fef7afd307be597f1f77e7eb8a8e0ccff565
Ilya Hatsuk <i.hatsuk@yahoo.com>
2023-08-28 17:49:16 +0300

    add Lesson7

---
+++ Lesson7/Dentist.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+class Dentist extends Doctor
+{
+    @Override
+    public void heal() {
+        System.out.println("Dentist is healing your teeth");
+    }
+}
---
+++ Lesson7/Patient.java
@@ -0,0 +1,23 @@
+package Lesson7;
+
+class Patient
+{
+    int healPlan;
+    String doctor;
+
+    public int getHealPlan() {
+        return healPlan;
+    }
+
+    public void setHealPlan(int healPlan) {
+        this.healPlan = healPlan;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+}
