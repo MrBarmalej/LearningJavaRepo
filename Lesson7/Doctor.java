fd3741ad95ddd48acc9aca4f262c103fe9b98084
Ilya Hatsuk <i.hatsuk@yahoo.com>
2023-08-28 17:56:40 +0300

    add Lesson7

---
+++ Lesson7/Doctor.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+class Doctor
+{
+    public void heal()
+    {
+        System.out.println("Who is goning to heal?");
+    }
+}
---
+++ Lesson7/Surgeon.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+class Surgeon extends Doctor
+{
+    @Override
+    public void heal() {
+        System.out.println("Surgeon is healing your injuries");
+    }
+}
